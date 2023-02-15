35d5814de6d077e09a455e798c03b8827021d892
Zhannel Omarova <98833988+zhonniks@users.noreply.github.com>
2023-02-15 18:16:59 +0600

    Create Rewrite "if" into "switch".js

---
+++ LAB3/JavaScript info/JavaScript Fundamentals/The "switch" statement/Rewrite "if" into "switch".js
@@ -0,0 +1,17 @@
+let x = +prompt('a?', '');
+
+switch (x) {
+  case 0:
+    alert( 0 );
+    break;
+
+  case 1:
+    alert( 1 );
+    break;
+
+  case 2:
+  
+  case 3:
+    alert( '2,3' );
+    break;
+}
