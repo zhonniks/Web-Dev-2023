f3bb6a5d6f6affdffa128b0200a8eb39b903785f
Zhannel Omarova <98833988+zhonniks@users.noreply.github.com>
2023-02-15 18:43:57 +0600

    Create string.js

---
+++ LAB3/JavaScript info/Data types/string.js
@@ -0,0 +1,32 @@
+function ucFirst(str) {
+    if (!str) return str;
+
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+alert( ucFirst("john") ); // John
+
+// 2
+
+function checkSpam(str) {
+    let lowerStr = str.toLowerCase();
+
+    return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+}
+
+alert( checkSpam('buy ViAgRA now') );
+alert( checkSpam('free xxxxx') );
+alert( checkSpam("innocent rabbit") );
+
+// 3
+
+function truncate(str, maxlength) {
+    return (str.length > maxlength) ?
+        str.slice(0, maxlength - 1) + '…' : str;
+}
+
+// 4
+
+function extractCurrencyValue(str) {
+    return +str.slice(1);
+}
