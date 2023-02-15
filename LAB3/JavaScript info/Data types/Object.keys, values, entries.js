3bbdac328ba8be5b624b939e120dfb7c18cf09a5
Zhannel Omarova <98833988+zhonniks@users.noreply.github.com>
2023-02-15 18:49:14 +0600

    Create Object.keys, values, entries.js

---
+++ LAB3/JavaScript info/Data types/Object.keys, values, entries.js
@@ -0,0 +1,16 @@
+function sumsal(salaries) {
+
+  let sum = 0;
+  for (let salary of Object.values(salaries)) {
+    sum += salary;
+  }
+
+  return sum;
+}
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+alert( sumsal(salaries) ); 
