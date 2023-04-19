17709e4c28280de45291328436a62970bf3bf1cc
Zhannel Omarova <98833988+zhonniks@users.noreply.github.com>
2023-04-19 22:34:23 +0600

    Add files via upload

---
+++ LAB10/front/src/app/companies/companies.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Company} from "../models";
+import {CompaniesServiceService} from "../companies-service.service";
+
+@Component({
+  selector: 'app-companies',
+  templateUrl: './companies.component.html',
+  styleUrls: ['./companies.component.css']
+})
+export class CompaniesComponent implements OnInit {
+
+  companies: Company[] = []
+
+  constructor(private companyService: CompaniesServiceService) { }
+
+  ngOnInit(): void {
+    this.getCompanies();
+  }
+
+  getCompanies(){
+    this.companyService.getCompanies().subscribe((data) => {
+      this.companies = data;
+    })
+  }
+
+}
